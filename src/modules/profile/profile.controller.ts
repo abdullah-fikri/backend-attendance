2775f1280a656bbe77cf4c8d2b9a4d605336d0ff
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-12-11 17:52:12 +0700

    feat: update profile controller

--- src/modules/profile/profile.controller.ts
+++ src/modules/profile/profile.controller.ts
@@ -4,17 +4,14 @@ import {
   HttpException,
   HttpStatus,
   Req,
-  UseGuards,
 } from '@nestjs/common';
 import { ResponseMessage } from 'src/common/decorators/response-message.decorator';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { UsersService } from '../users/users.service';
 
 @Controller('profile')
 export class ProfileController {
   constructor(private readonly usersService: UsersService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Get('')
   @ResponseMessage('Success get data profile user')
   getProfile(@Req() req) {
