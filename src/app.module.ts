aa7755b96f8df7789ad1c4442402fac65351abac
ari eka saputra <arieka310107@gmail.com>
2025-12-11 11:15:03 +0700

    feat: create function login will return a token containing the userid and role_id

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './config/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtConfigModule } from './config/JwtConfigModule';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, JwtConfigModule],
 })
 export class AppModule {}
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtConfigModule } from 'src/config/JwtConfigModule';
 
 @Module({
+  imports: [JwtConfigModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
