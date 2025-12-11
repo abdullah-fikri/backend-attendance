61b5a3c347057e15e5977d5dc471c39acff11791
ari eka saputra <arieka310107@gmail.com>
2025-12-11 11:52:46 +0700

    fix : Correcting the JWT config error that does not read the environment

--- src/config/JwtConfigModule.ts
+++ src/config/JwtConfigModule.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [
-    JwtModule.register({
-      secret: process.env.APP_SECRET,
-      signOptions: { expiresIn: '1d' },
-    }),
-  ],
-  exports: [JwtModule],
-})
-export class JwtConfigModule {}
+    imports: [
+      JwtModule.registerAsync({
+        imports: [ConfigModule],
+        useFactory: (config: ConfigService) => ({
+          secret: config.get<string>('APP_SECRET'),
+          signOptions: { expiresIn: '1d' },
+        }),
+        inject: [ConfigService],
+      }),
+    ],
+    exports: [JwtModule],
+  })
+  export class JwtConfigModule {}
