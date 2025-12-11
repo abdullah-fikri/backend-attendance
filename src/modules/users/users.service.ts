cf134cbb950e3131b2e4a6b91beb30f63eb88c37
abdullah-fikri <abdullahfikri981@gmail.com>
2025-12-11 08:30:06 +0000

    feat: query for get all users support hateoas and pagination

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -2,7 +2,10 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/config/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { GetUsersDto } from './dto/get-users.dto';
+import { buildMeta, buildPagination } from 'src/common/utils/pagination.util';
 import { UserMapper } from './mappers/user-mapper';
+import { hateoas } from 'src/common/utils/hateoas.util';
 
 @Injectable()
 export class UsersService {
@@ -11,8 +14,41 @@ export class UsersService {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(query: GetUsersDto, baseUrl: string) {
+    const {page, limit, skip, take} = buildPagination(query)
+    
+    const where: any = {}
+
+    if (query.search){
+      where.OR =[
+        {email: {contains: query.search, mode: 'insensitive'}},
+        {fullName: {contains: query.search, mode: 'insensitive'}},
+      ];
+    }
+
+    if (query.role){
+      where.role = {name: query.role}
+    }
+
+    const [data, total] =  await Promise.all([
+      this.prisma.user.findMany({
+        where,
+        skip,
+        take,
+        include: {role: true},
+        orderBy: {createdAt: 'desc'}
+      }),
+      this.prisma.user.count({where})
+    ]);
+
+    const meta = buildMeta(page, limit, total);
+    const links = hateoas(baseUrl, meta.page, meta.limit, meta.totalPages);
+  
+    return {
+      meta,
+      links,
+      data: data.map((user) => UserMapper.toResponse(user)),
+    }
   }
 
   async findOne(id: string) {
