2155a0a0190e3fccc9e67d66123a2c04195a7209
federus1105 <federusrudi@gmail.com>
2025-12-11 10:30:04 +0700

    Feat : register function decorators globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './config/prisma.module';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './common/guards/role.guard';
 
 @Module({
+  providers: [{ 
+    provide: APP_GUARD,
+    useClass: RolesGuard
+  }],
   imports: [ConfigModule.forRoot({ isGlobal: true, 
     envFilePath: 
       process.env.NODE_ENV === 'production' ? '.env.production' : '.env' }), 
