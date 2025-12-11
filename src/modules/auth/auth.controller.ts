d81ba04b38697319cd33034dcb140ff473fd200e
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-12-11 17:51:26 +0700

    feat: adjust auth controller (added public decorator)

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Post, Body, Res } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { ResponseMessage } from 'src/common/decorators/response-message.decorator';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import type { Response } from 'express';
+import { Public } from 'src/common/decorators/public.decorator';
+import { ResponseMessage } from 'src/common/decorators/response-message.decorator';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @Public()
   @ResponseMessage('success to login')
   async login(
     @Body() body: { email: string; password: string },
@@ -25,6 +27,7 @@ export class AuthController {
   }
 
   @Post('logout')
+  @Public()
   @ResponseMessage('success to logout')
   async logout(@Res({ passthrough: true }) res: Response) {
     res.clearCookie('accessToken', {
