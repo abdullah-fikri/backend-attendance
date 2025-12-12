811896d47e17e834d373103aec08ee2eaa283c44
federus1105 <federusrudi@gmail.com>
2025-12-12 16:33:25 +0700

    Feat ( attendance ) : feature service for attendance clock in

--- src/modules/attendance/attendance.service.ts
+++ src/modules/attendance/attendance.service.ts
@@ -1,26 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateAttendanceDto } from './dto/create-attendance.dto';
-import { UpdateAttendanceDto } from './dto/update-attendance.dto';
+import { PrismaService } from 'src/config/prisma.service';
+import { AttendanceStatus } from 'generated/prisma/enums';
 
 @Injectable()
 export class AttendanceService {
-  create(createAttendanceDto: CreateAttendanceDto) {
-    return 'This action adds a new attendance';
+  constructor(private readonly prisma: PrismaService) {}
+
+  // --- CLOCK IN ---
+  async clockIn(userId: string, createAttendanceDto: CreateAttendanceDto) {
+    const { latIn, longIn } = createAttendanceDto;
+
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    
+    const existing = await this.prisma.attendance.findFirst({
+      where: {
+        userId,
+        date: today,
+      },
+    });
+    if (existing) {
+      throw new  BadRequestException('You ve already clocked in today');
+    }
+
+    // --- SET STATUS ---
+    const now = new Date();
+    const officeStartHour = 9;
+    const status = now.getHours() < officeStartHour ? AttendanceStatus.ON_TIME : AttendanceStatus.LATE;
+
+      
+    // --- SAVE ATTENDANCE --- 
+    const attendance = await this.prisma.attendance.create({
+      data: {
+        userId,
+        date: today,
+        clockInTime: now,
+        latIn,
+        longIn,
+        status,
+      },
+    // --- SET RESPONSE ---
+    select: {
+      id: true,
+      userId: true,
+      date: true,
+      clockInTime: true,
+      clockOutTime: true,
+      latIn: true,
+      longIn: true,
+      latOut: true,
+      longOut: true,
+      status: true,
+    },
+    });
+
+
+    return attendance;
   }
 
-  findAll() {
-    return `This action returns all attendance`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} attendance`;
-  }
-
-  update(id: number, updateAttendanceDto: UpdateAttendanceDto) {
-    return `This action updates a #${id} attendance`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} attendance`;
-  }
 }
