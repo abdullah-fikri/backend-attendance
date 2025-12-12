aa85362d37e50b673750d25fc02cc892d2c328f9
federus1105 <federusrudi@gmail.com>
2025-12-12 16:32:22 +0700

    Feat ( attendance ) : feature controller attendance clock-in

--- src/modules/attendance/attendance.controller.ts
+++ src/modules/attendance/attendance.controller.ts
@@ -1,34 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body, Req, BadRequestException, HttpException, UnauthorizedException, } from '@nestjs/common';
 import { AttendanceService } from './attendance.service';
 import { CreateAttendanceDto } from './dto/create-attendance.dto';
-import { UpdateAttendanceDto } from './dto/update-attendance.dto';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { ResponseMessage } from 'src/common/decorators/response-message.decorator';
 
 @Controller('attendance')
+@Roles("EMPLOYEE")
 export class AttendanceController {
   constructor(private readonly attendanceService: AttendanceService) {}
 
-  @Post()
-  create(@Body() createAttendanceDto: CreateAttendanceDto) {
-    return this.attendanceService.create(createAttendanceDto);
-  }
+  @Post('clock-in')
+  @ResponseMessage("Created Attendance successfully")
+  async clockIn(@Req() req, @Body() body: CreateAttendanceDto) {
+    const userId = req.user?.userId;
+    
+    const { latIn, longIn } = body;
+    if (latIn == null || longIn == null) {
+      throw new BadRequestException('Latitude and Longitude are required');
+    }
 
-  @Get()
-  findAll() {
-    return this.attendanceService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.attendanceService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAttendanceDto: UpdateAttendanceDto) {
-    return this.attendanceService.update(+id, updateAttendanceDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.attendanceService.remove(+id);
-  }
+    return this.attendanceService.clockIn(userId, body);
 }
+}
+  
