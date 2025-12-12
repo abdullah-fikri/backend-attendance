70ccf043d21e1ad25e012aac09ee99d1b7400236
abdullah-fikri <abdullahfikri981@gmail.com>
2025-12-12 10:36:57 +0000

    feat: module attendance

--- src/modules/attendance/attendance.module.ts
+++ src/modules/attendance/attendance.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtConfigModule } from 'src/config/JwtConfigModule';
+import { JwtStrategy } from '../auth/strategies/jwt.strategy';
 import { AttendanceService } from './attendance.service';
 import { AttendanceController } from './attendance.controller';
 
 @Module({
+  imports: [PassportModule, JwtConfigModule],
   controllers: [AttendanceController],
-  providers: [AttendanceService],
+  providers: [AttendanceService, JwtStrategy],
 })
 export class AttendanceModule {}
