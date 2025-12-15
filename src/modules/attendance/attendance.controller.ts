54952f7fdd6601dcbcc5ff15773fed0ecaed43de
abdullah-fikri <abdullahfikri981@gmail.com>
2025-12-15 02:43:56 +0000

    fix: endpoint post attendance

--- src/modules/attendance/attendance.controller.ts
+++ src/modules/attendance/attendance.controller.ts
@@ -1,37 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { Controller, Post, Body, Req, BadRequestException, HttpException, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { AttendanceService } from './attendance.service';
 import { CreateAttendanceDto } from './dto/create-attendance.dto';
-import { UpdateAttendanceDto } from './dto/update-attendance.dto';
-import { AuthGuard } from '@nestjs/passport';
 import { Roles } from 'src/common/decorators/roles.decorator';
+import { ResponseMessage } from 'src/common/decorators/response-message.decorator';
 
 @Controller('attendance')
+@Roles("ADMIN")
 export class AttendanceController {
   constructor(private readonly attendanceService: AttendanceService) {}
 
-  @Post()
-  create(@Body() createAttendanceDto: CreateAttendanceDto) {
-    return this.attendanceService.create(createAttendanceDto);
-  }
+  @Post('clock-in')
+  @ResponseMessage("Created Attendance successfully")
+  async clockIn(@Req() req, @Body() body: CreateAttendanceDto) {
+    const userId = req.user?.userId;
+    
+    const { latIn, longIn } = body;
+    if (latIn == null || longIn == null) {
+      throw new BadRequestException('Latitude and Longitude are required');
+    }
 
-  @Get()
-  findAll() {
-    return this.attendanceService.findAll();
-  }
-
-  @Get('today')
-  @Roles("EMPLOYEE")
-  async findOne(@Req() req) {
-    return this.attendanceService.findOne(req.user.userId);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAttendanceDto: UpdateAttendanceDto) {
-    return this.attendanceService.update(+id, updateAttendanceDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.attendanceService.remove(+id);
-  }
+    return this.attendanceService.clockIn(userId, body);
 }
+}
+  
--- src/modules/attendance/attendance.service.ts
+++ src/modules/attendance/attendance.service.ts
@@ -1,89 +1,78 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { CreateAttendanceDto } from './dto/create-attendance.dto';
-import { UpdateAttendanceDto } from './dto/update-attendance.dto';
+import {
+  BadRequestException,
+  ForbiddenException,
+  HttpException, HttpStatus, Injectable,
+} from '@nestjs/common';
+import { AttendanceStatus } from 'generated/prisma/enums';
+import { haversineDistance } from 'src/common/utils/haversine.util';
 import { PrismaService } from 'src/config/prisma.service';
+import { CreateAttendanceDto } from './dto/create-attendance.dto';
 
 @Injectable()
 export class AttendanceService {
-  constructor(private prisma: PrismaService) {}
-  create(createAttendanceDto: CreateAttendanceDto) {
-    return 'This action adds a new attendance';
-  }
+  constructor(private readonly prisma: PrismaService) {}
 
-  findAll() {
-    return `This action returns all attendance`;
-  }
+  // --- CLOCK IN ---
+  async clockIn(userId: string, createAttendanceDto: CreateAttendanceDto) {
+    const { latIn, longIn } = createAttendanceDto;
+    const officeLat = parseFloat(process.env.OFFICE_LAT || '');
+    const officeLong = parseFloat(process.env.OFFICE_LONG || '');
+    const officeRadius = parseInt(process.env.OFFICE_RADIUS || '');
 
-  async findOne(id: string) {
-    const today = new Date();
-    const start = new Date(
-      today.getFullYear(),
-      today.getMonth(),
-      today.getDate(),
-      0,
-      0,
-      0,
-    );
-    const end = new Date(
-      today.getFullYear(),
-      today.getMonth(),
-      today.getDate(),
-      23,
-      59,
-      59,
-    );
+    const distance = haversineDistance(latIn, longIn, officeLat, officeLong);
 
-    const attendance = await this.prisma.attendance.findFirst({
-      where: {
-        id,
-        date: {
-          gte: start,
-          lte: end,
-        },
-      },
-      include: {
-        user: true
-      }
-    });
-
-    if (!attendance) {
-      throw new HttpException(
-        {
-          message: 'not found with the user',
-          data: {
-            date: start.toISOString().split('T')[0],
-            status: 'ABSENT',
-            clockInTime: null,
-            clockOutTime: null
-          },
-        },
-        HttpStatus.NOT_FOUND,
+    if (distance > officeRadius) {
+      throw new ForbiddenException(
+        `You are outside the attendance radius. Distance: ${distance.toFixed()} meters. Radius max: ${officeRadius} meters.`,
       );
     }
-    return {
-      fullName: attendance.user.fullName,
-      date: attendance.date,
-      status: attendance.status,
-      clockInTime: attendance.clockInTime,
-      clockOutTime: attendance.clockOutTime,
-      location: {
-        clockIn: {
-          lat: attendance.latIn,
-          long: attendance.longIn
-        },
-        clockOut: {
-          lat: attendance.latOut,
-          long: attendance.longOut
-        }
-      }
-    };
-  }
 
-  update(id: number, updateAttendanceDto: UpdateAttendanceDto) {
-    return `This action updates a #${id} attendance`;
-  }
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
 
-  remove(id: number) {
-    return `This action removes a #${id} attendance`;
+    const existing = await this.prisma.attendance.findFirst({
+      where: {
+        userId,
+        date: today,
+      },
+    });
+    if (existing) {
+      throw new BadRequestException('You ve already clocked in today');
+    }
+
+    // --- SET STATUS ---
+    const now = new Date();
+    const officeStartHour = 9;
+    const status =
+      now.getHours() < officeStartHour
+        ? AttendanceStatus.ON_TIME
+        : AttendanceStatus.LATE;
+
+    // --- SAVE ATTENDANCE ---
+    const attendance = await this.prisma.attendance.create({
+      data: {
+        userId,
+        date: today,
+        clockInTime: now,
+        latIn,
+        longIn,
+        status,
+      },
+      // --- SET RESPONSE ---
+      select: {
+        id: true,
+        userId: true,
+        date: true,
+        clockInTime: true,
+        clockOutTime: true,
+        latIn: true,
+        longIn: true,
+        latOut: true,
+        longOut: true,
+        status: true,
+      },
+    });
+
+    return attendance;
   }
 }
