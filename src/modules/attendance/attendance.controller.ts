f63eac42b97372a6461f5bb10bd4c0a4d1141617
abdullah-fikri <abdullahfikri981@gmail.com>
2025-12-15 03:05:02 +0000

    feat: added controller for get attendance today

--- src/modules/attendance/attendance.controller.ts
+++ src/modules/attendance/attendance.controller.ts
@@ -1,25 +1,38 @@
-import { Controller, Post, Body, Req, BadRequestException, HttpException, UnauthorizedException, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  Req,
+  BadRequestException,
+} from '@nestjs/common';
 import { AttendanceService } from './attendance.service';
 import { CreateAttendanceDto } from './dto/create-attendance.dto';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { ResponseMessage } from 'src/common/decorators/response-message.decorator';
 
 @Controller('attendance')
-@Roles("ADMIN")
+@Roles('EMPLOYEE')
 export class AttendanceController {
   constructor(private readonly attendanceService: AttendanceService) {}
 
   @Post('clock-in')
-  @ResponseMessage("Created Attendance successfully")
+  @ResponseMessage('Created Attendance successfully')
   async clockIn(@Req() req, @Body() body: CreateAttendanceDto) {
     const userId = req.user?.userId;
-    
+
     const { latIn, longIn } = body;
     if (latIn == null || longIn == null) {
       throw new BadRequestException('Latitude and Longitude are required');
     }
 
     return this.attendanceService.clockIn(userId, body);
+  }
+
+  @Get('today')
+  @ResponseMessage('Get Attendance today success')
+  @Roles('EMPLOYEE')
+  async findOne(@Req() req) {
+    return this.attendanceService.findOne(req.user.userId);
+  }
 }
-}
-  
