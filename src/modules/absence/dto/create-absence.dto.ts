41ed9e0147aa6439e60e9c1a599728eaa676c45e
abdullah-fikri <abdullahfikri981@gmail.com>
2025-12-17 06:50:34 +0000

    feat: validator for create post absence

--- src/modules/absence/dto/create-absence.dto.ts
+++ src/modules/absence/dto/create-absence.dto.ts
@@ -1,1 +1,21 @@
-export class CreateAbsenceDto {}
+
+import { IsDateString, IsEnum, IsOptional, IsString } from 'class-validator';
+import { AbsenceType } from 'generated/prisma/enums';
+
+export class CreateAbsenceDto {
+  @IsEnum(AbsenceType)
+  type: AbsenceType;
+
+  @IsDateString()
+  startDate: string;
+
+  @IsDateString()
+  endDate: string;
+
+  @IsString()
+  reason: string;
+
+  @IsOptional()
+  @IsString()
+  attachmentUrl?: string;
+}
