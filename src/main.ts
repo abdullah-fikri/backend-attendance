2451f5d8984f7ae7729c9f6b0ccaf82e46493c1a
federus1105 <federusrudi@gmail.com>
2025-12-19 13:31:41 +0700

    Feat : feature security header helmet

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/filters/global.exception.filter';
 import { ResponseInterceptor } from './common/filters/response.interceptors';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalFilters(new GlobalExceptionFilter());
 
   app.useGlobalPipes(
