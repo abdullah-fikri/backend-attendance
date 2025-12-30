51f2de319114858e07b30c695156391bb0641b59
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-12-30 14:23:03 +0700

    fix: migrattion and schema prisma

--- prisma/migrations/20251223042221_add_field_leave_balance/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "leave_balance" INTEGER NOT NULL DEFAULT 14;
-
--- CreateIndex
-CREATE INDEX "attendances_status_created_at_user_id_idx" ON "attendances"("status", "created_at", "user_id");
-
--- CreateIndex
-CREATE INDEX "users_full_name_idx" ON "users"("full_name");
