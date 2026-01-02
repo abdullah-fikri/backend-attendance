a26050d45b2f45c024ff3eb71d4a004ea1d46f57
abdullah-fikri <abdullahfikri981@gmail.com>
2026-01-02 15:55:58 +0000

    fix: update controller post added roles guard

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -24,6 +24,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService, private readonly excelService: GenerateExcel,) {}
 
   @Post()
+  @Roles('ADMIN')
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
