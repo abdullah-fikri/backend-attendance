fbc619a4cf694c2e86ed08b77df9cf4c99e40847
abdullah-fikri <abdullahfikri981@gmail.com>
2026-01-05 08:26:56 +0000

    feat: create endpoint get report monthly for admin

--- src/modules/reports/reports.service.ts
+++ src/modules/reports/reports.service.ts
@@ -99,4 +99,92 @@ export class ReportsService {  constructor(private prisma: PrismaService) {}
     meta: buildMeta(pagination.page, pagination.limit, total),
   };
   }
+
+  async getMonthlyAnalytics(params: {
+    month?: string;
+    startDate?: string;
+    endDate?: string;
+  }) {
+    const now = new Date();
+  
+    let start: Date;
+    let end: Date;
+  
+    /** =========================
+     *  PRIORITY:
+     *  1. startDate + endDate
+     *  2. month
+     *  3. current month
+     *  ========================= */
+    if (params.startDate && params.endDate) {
+      start = new Date(params.startDate);
+      end = new Date(params.endDate);
+      end.setHours(23, 59, 59);
+    } else if (params.month) {
+      const target = new Date(`${params.month}-01`);
+      start = new Date(target.getFullYear(), target.getMonth(), 1);
+      end = new Date(target.getFullYear(), target.getMonth() + 1, 0, 23, 59, 59);
+    } else {
+      start = new Date(now.getFullYear(), now.getMonth(), 1);
+      end = new Date(now.getFullYear(), now.getMonth() + 1, 0, 23, 59, 59);
+    }
+  
+    //summary
+    const summaryRaw = await this.prisma.attendance.groupBy({
+      by: ['status'],
+      where: {
+        date: { gte: start, lte: end },
+      },
+      _count: { status: true },
+    });
+  
+    const summary = {
+      ON_TIME: 0,
+      LATE: 0,
+      ABSENT: 0,
+      TOTAL: 0,
+    };
+  
+    summaryRaw.forEach((row) => {
+      summary[row.status] = row._count.status;
+      summary.TOTAL += row._count.status;
+    });
+  
+    // trend
+    const trendRaw = await this.prisma.attendance.groupBy({
+      by: ['date', 'status'],
+      where: {
+        date: { gte: start, lte: end },
+      },
+      _count: { status: true },
+      orderBy: { date: 'asc' },
+    });
+  
+    const trendMap = new Map<string, any>();
+  
+    for (const row of trendRaw) {
+      const dateKey = row.date.toISOString().split('T')[0];
+  
+      if (!trendMap.has(dateKey)) {
+        trendMap.set(dateKey, {
+          date: dateKey,
+          ON_TIME: 0,
+          LATE: 0,
+          ABSENT: 0,
+        });
+      }
+  
+      trendMap.get(dateKey)[row.status] = row._count.status;
+    }
+  
+    return {
+      range: {
+        start: start.toISOString().split('T')[0],
+        end: end.toISOString().split('T')[0],
+      },
+      summary,
+      trend: Array.from(trendMap.values()),
+    };
+  }
+  
 }
