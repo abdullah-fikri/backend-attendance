eed7a36913823e4427e04a2b03fccf5cda60c21b
ari <arieka310107@gmail.com>
2026-01-05 10:18:36 +0700

    feat: create docs swagger in end point attendance

--- src/modules/attendance/dto/create-attendance.dto.ts
+++ src/modules/attendance/dto/create-attendance.dto.ts
@@ -1,17 +1,38 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import { IsNumber } from 'class-validator';
 
 export class CreateAttendanceDto {
+  @ApiProperty({
+    example: -6.20000012,
+    description: 'Latitude saat clock in',
+  })
   @Transform(({ value }) => Number(Number(value).toFixed(8)))
   @IsNumber()
   latIn: number;
 
+  @ApiProperty({
+    example: 106.81666654,
+    description: 'Longitude saat clock in',
+  })
   @Transform(({ value }) => Number(Number(value).toFixed(8)))
   @IsNumber()
   longIn: number;
 }
 
-export class CreateClockOutDto{
+export class CreateClockOutDto {
+
+  @ApiProperty({
+    example: -6.20000012,
+    description: 'Latitude saat clock out',
+  })
+  @IsNumber()
   latOut: number;
+
+  @ApiProperty({
+    example: 106.81666654,
+    description: 'Longitude saat clock out',
+  })
+  @IsNumber()
   longOut: number;
 }
